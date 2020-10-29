b4f1811b0bbbd7856be8917d426015cb47a35ba6
MarceloYevenes <yevenesmarcelo1@gmail.com>
2020-10-29 19:29:16 -0300

    Añadido elementos basicos del array

--- Array/app.js
+++ Array/app.js
@@ -39,10 +39,11 @@ const indice = paises.findIndex((variable)=> variable==='Argentina')
 console.log(indice);
 
 //Filtra un array y retorna un array con elementos que cumplan la condicion
+//Importante se puede concatenar los filter filter(funcion).filter(funcion2) etc
 const filtrado = paises.filter((pais)=>{
     return pais === 'Chile';
 })
-//Solo muestra Chile
+//Solo muestra Chile sino encuentra nada array vacio
 console.log(filtrado);
 
 
@@ -52,6 +53,72 @@ const elementoSome = paises.some((pais)=>{
 });
 //Retorna true porque si existe
 console.log(elementoSome);
+
+
+//Retorna el primer elemento que cumpla cierta condicion
+const paisCondicion = paises.find(pais => pais === 'Chile');
+
+//Retorna el elemento Chile sino undefine
+console.log(paisCondicion);
+
+//Evalua una condicion en todos los elementos
+//Retorna true si todos los elementos la cumplen sino false
+const paisEvery = paises.every((pais)=> pais==='Chile');
+console.log(paisEvery);
+
+
+//Concatenar dos array
+const numeros1 = ['uno','dos','tres'];
+const numeros2 = ['cuatro','cinco','seis'];
+
+//2 formas de concatenar - se pueden hacer con n arrays
+console.log([...numeros1,...numeros2]);
+//Se puede hacer con n arrays separando con una coma concat(numeros1, numeros2, numeros3)
+console.log(numeros1.concat(numeros2));
+
+
+//Muestra el tamaño de un array en este caso 4(0,1,2,3)
+console.log(paises.length);
+
+//Agregar, eliminar elementos
+let elementoArray = ['primero','segundo', 'tercero', 'cuarto'];
+
+//Agregar un elemento al incio del array
+elementoArray.unshift('Ante-primero');
+console.log(elementoArray);
+
+//Agregar un elemento al final del array
+elementoArray.push('quinto');
+console.log(elementoArray);
+
+//Eliminar un elemento al incio del array
+elementoArray.shift('quinto');
+console.log(elementoArray);
+
+//Eliminar un elemento al final del array
+elementoArray.pop('quinto');
+console.log(elementoArray);
+
+
+//Poner lo elementos en una cadena separandolos por un delimitador 
+
+//Saldra primero,segundo,tercero,cuarto
+console.log(elementoArray.join());
+
+//Saldra primero-segundo-tercero-cuarto
+console.log(elementoArray.join('-'));
+
+
+//Cambiar el orden de los elementos de un array(cuarto, tercero, segundo, primero)
+console.log(elementoArray.reverse());
+
+
+//Devolver los elementos seleccionados de inicio - fin 
+console.log(elementoArray.slice(0-4));
+
+
+
+
 
 
 
