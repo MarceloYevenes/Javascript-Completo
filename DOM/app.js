3c9f4d947c76614a2b0bb6434aee9df72f297431
MarceloYevenes <yevenesmarcelo1@gmail.com>
2020-11-01 15:57:54 -0300

    Agregada algunas funciones para el DOM

---
+++ DOM/app.js
@@ -0,0 +1,20 @@
+//Seleccionar elementos
+
+//Seleccionar una clase
+const cuerpo = document.getElementsByClassName('cuerpo')
+console.log(cuerpo);
+
+//Seleccionar elemento por su ID
+const titulo = document.getElementById('titulo')
+console.log(titulo);
+
+//Selector general (clases e ID)
+
+//Para clases
+const generalCuerpo = document.querySelector('.cuerpo')
+console.log(generalCuerpo);
+
+//Para ID
+const generalTitulo = document.querySelector('#titulo')
+console.log(generalTitulo);
+
